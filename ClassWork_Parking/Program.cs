7cf3b37d3da1f5685cea127cb57e02355e46f0fe
Владислав Павлов <vlpavlov58@gmail.com>
2018-11-16 19:10:02 +0200

    Add project files.

---
+++ ClassWork_Parking/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassWork_Parking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
